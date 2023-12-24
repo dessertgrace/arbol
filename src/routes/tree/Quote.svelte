fb042a0d67ff04422760b03bd0c5071cd212de52
dessertgrace <dessertgrace@gmail.com>
2023-12-24 15:38:32 -0800

    fix quote

--- src/routes/tree/Quote.svelte
+++ src/routes/tree/Quote.svelte
@@ -3,30 +3,27 @@
 
     let quote = {};
 
-    export async function load(reload=false) {
-        console.log("loading quote");
+    console.log("loading quote");
 
-        // if quote already loaded and not requested new, quit
-        // if (quote.content && !reload) {
-        //     return;
-        // }
+    // if quote already loaded and not requested new, quit
+    // if (quote.content && !reload) {
+    //     return;
+    // }
 
-        let res = await fetch(`https://api.quotable.io/random`);
-        let data = await res.json();
-        console.log(data);
-        
+    let res = await fetch(`https://api.quotable.io/random`);
+    let data = await res.json();
+    console.log(data);
+    
 
-        if (!res.ok || !data) {
-            console.error('Failed to fetch quote:', data);
-            let data = {'author': 'Unknown', 'content': 'Kindness is the language that the deaf can hear and the blind can see'}
-        console.log(data);
-        }
-
-        quote.content = data.content;
-        quote.author = data.author;
+    if (!res.ok || !data) {
+        console.error('Failed to fetch quote:', data);
+        let data = {'author': 'Unknown', 'content': 'Kindness is the language that the deaf can hear and the blind can see'}
+    console.log(data);
     }
 
-    load();
+    quote.content = data.content;
+    quote.author = data.author;
+
 </script>
 
 <section>
